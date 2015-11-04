7f7f0028aa7169f64428b315b2d4a2a34647a02a
Matthew McClain <MatthewMcClain17@users.noreply.github.com>
2015-11-03 23:35:22 -0800

    Code Overhaul Part 1
    + organized all the code that goes before setup()
    + Added various global variables to make tweaking settings easier
    + Added testing code (currently commented out)
    + Fixed capacitive sensor checks in loop()
    + Began rewriting action1() to serve as a model for the other two action functions

--- Bake_Sale_Robot.ino
+++ Bake_Sale_Robot.ino
@@ -12,76 +12,110 @@
  -ADVANCED: Code interrupts for calibration or emergency shutdowns
 */
 
+// LIBRARIES
 #include <Servo.h>
 #include <Stepper.h>
 #include <CapacitiveSensor.h> //make sure you have this installed, found at:
 //http://playground.arduino.cc/Main/CapacitiveSensor?from=Main.CapSense
 
-CapacitiveSensor Cookie = CapacitiveSensor(48, 49);
-CapacitiveSensor Donut = CapacitiveSensor(50, 51);
-CapacitiveSensor Muffin = CapacitiveSensor(52, 53);
 
-const int distSensor = 15; //Infrared distance sensor signal pin
+// SETTINGS AND THRESHOLDS
+const int distSensor = 7; //Infrared distance sensor signal pin
+const int stepsPerRevolution = 200;  // number of steps per revolution for our motors
+// Servo limits – to prevent the claw from breaking itself, stay near these angles
+const int clawNeutralAngle = 100;
+const int clawOpenAngle = 150;
+const int clawClosedAngle = 80;
+// Capacitive sensor activation thresholds
+const int cookieThreshold = 100;
+const int donutThreshold = 100;
+const int muffinThreshold = 100;
+const int distSenseThreshold = 500; // Distance sensor hand sensing threshold
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-// for your motor
 
-
-// initialize the stepper library on pins 8 through 11:
+// OBJECT INITIALIZATION
+// initialize capacitive sensors
+CapacitiveSensor Cookie = CapacitiveSensor(48, 49); // Green wire?
+CapacitiveSensor Donut = CapacitiveSensor(50, 51); // Red wire?
+CapacitiveSensor Muffin = CapacitiveSensor(52, 53); // Black wire?
+// initialize stepper motors
 Stepper topStepper(stepsPerRevolution, 22, 24, 26, 28);
 Stepper bottomStepper(stepsPerRevolution, 23, 25, 27, 29);
 // initialize claw servo
 Servo clawServo;
 
+
+
 void setup() {
   pinMode(distSensor, INPUT); // set the distance sensor's signal to input
-  // set the speed at 30 rpm:
 
-  bottomStepper.setSpeed(30);
+ // set the rpm of the stepper
   topStepper.setSpeed(30);
+  bottomStepper.setSpeed(5);
 
-  clawServo.attach(43);
+  clawServo.attach(4);
+  clawServo.write(clawNeutralAngle);
 
-  // initialize the serial port:
+  // initialize the serial port for testing purposes:
   Serial.begin(9600);
 }
 
 void loop() {
-
-  if (Cookie.CapacitiveSensor > 100) { // if Cookie is greater than 100 (an arbitrary threshold)
+ // FOR TESTING PURPOSES
+   /* Distance sensor testing
+  Serial.println(analogRead(distSensor));
+  delay(500);
+  */
+  /* Claw servo testing
+  clawServo.write(180);
+  delay(1000);
+  clawServo.write(100);
+  delay(1000);
+  */
+  /* Stepper testing
+  bottomStepper.step(70);
+  delay(500);
+  bottomStepper.step(-70);
+  delay(500);
+  */
+ 
+ /*
+  if (Cookie.capacitiveSensor(30) > cookieThreshold) {
     action1();
   }
 
-  if (Donut.CapacitiveSensor > 100) { // if Donut is greater than 100 (an arbitrary threshold)
+  if (Donut.capacitiveSensor(30) > donutThreshold) {
   action2();
   }
 
-  if (Muffin.CapacitiveSensor > 100) { // if Muffin is greater than 100 (an arbitrary threshold)
+  if (Muffin.capacitiveSensor(30) > muffinThreshold) {
   action3();
   }
+  */
 }
 
 // FUNCTIONS
 
 void action1() {
 
-  clawServo.write(0); //sets the angle of the servo to 0 degrees
+  clawServo.write(clawNeutralAngle); //sets the servo to the neutral position
   topStepper.step(650); // decrease elevates; increase brings servo down
   // step one revolution (stepsPerRevolution = 200) clockwise
   bottomStepper.step(50);
+  clawServo.write(clawOpenAngle); // fully opens the claw
   topStepper.step(-250); // decrease elevates; increase brings servo down
   
-  clawServo.write(95); //176 is maximum vibration limit, 180 is absolute max
+  clawServo.write(clawClosedAngle);
   
   // step one revolution in the other direction:
   topStepper.step(200);
   bottomStepper.step(-50);
   
-  Serial.println(analogRead(distSensor)); //FIX THIS!!  
-  
-  clawServo.write(0); //176 is maximum vibration limit, 180 is absolute max
-
- 
+  if (analogRead(distSensor) > distSenseThreshold) { // if there is a hand above the distance sensor
+   clawServo.write(clawOpenAngle); // open the claw and drop the item
+   delay(3000);
+   clawServo.write(clawNeutralAngle);
+  }
 }
 
 void action2() {
